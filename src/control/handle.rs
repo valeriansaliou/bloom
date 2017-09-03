617e995250b37924d0835f6268336142b70f2e6f
Valerian Saliou <valerian@valeriansaliou.name>
2017-09-03 12:35:01 +0200

    Account for LINE_END_GAP
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- src/control/handle.rs
+++ src/control/handle.rs
@@ -37,9 +37,10 @@ enum ControlHandleMessageResult {
     Close,
 }
 
-const MAX_LINE_SIZE: usize = COMMAND_SIZE + ROUTE_HASH_SIZE + 2;
+const LINE_END_GAP: usize = 1;
+const MAX_LINE_SIZE: usize = COMMAND_SIZE + ROUTE_HASH_SIZE + LINE_END_GAP + 1;
 const HASH_VALUE_SIZE: usize = 10;
-const HASH_RESULT_SIZE: usize = 7 + ROUTE_HASH_SIZE + 1;
+const HASH_RESULT_SIZE: usize = 7 + ROUTE_HASH_SIZE + LINE_END_GAP + 1;
 const SHARD_DEFAULT: ControlShard = 0;
 const TCP_TIMEOUT_NON_ESTABLISHED: u64 = 20;
 
