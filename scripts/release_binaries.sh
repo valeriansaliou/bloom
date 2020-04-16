10626a46aaa06d9207f0b973e8dfe103e35b41a4
Valerian Saliou <valerian@valeriansaliou.name>
2020-04-16 13:23:13 +0200

    Fix arch denomination for Debian
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- scripts/release_binaries.sh
+++ scripts/release_binaries.sh
@@ -62,9 +62,9 @@ rc=0
 pushd "$BASE_DIR" > /dev/null
     echo "Executing release steps for Bloom v$BLOOM_VERSION..."
 
-    release_for_architecture "x86_64" "x86_64-unknown-linux-musl" && \
-        release_for_architecture "i686" "i686-unknown-linux-musl" && \
-        release_for_architecture "armv7" "armv7-unknown-linux-musleabihf"
+    release_for_architecture "amd64" "x86_64-unknown-linux-musl" && \
+        release_for_architecture "i386" "i686-unknown-linux-musl" && \
+        release_for_architecture "armhf" "armv7-unknown-linux-musleabihf"
     rc=$?
 
     if [ $rc -eq 0 ]; then
