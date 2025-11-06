e585ac63994250032472384e1007da419bea96b6
Valerian Saliou <valerian@valeriansaliou.name>
2025-11-06 15:20:26 +0100

    Bump dep: clap

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@
 #[macro_use]
 extern crate log;
 #[macro_use]
-extern crate clap;
-#[macro_use]
 extern crate lazy_static;
 #[macro_use]
 extern crate serde_derive;
@@ -39,7 +37,7 @@ use std::str::FromStr;
 use std::thread;
 use std::time::Duration;
 
-use clap::{App, Arg};
+use clap::{Arg, Command};
 use log::LevelFilter;
 
 use cache::store::{CacheStore, CacheStoreBuilder};
@@ -66,23 +64,25 @@ lazy_static! {
 }
 
 fn make_app_args() -> AppArgs {
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
+    let matches = Command::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .about(clap::crate_description!())
         .arg(
             Arg::new("config")
                 .short('c')
                 .long("config")
                 .help("Path to configuration file")
-                .default_value("./config.cfg")
-                .takes_value(true),
+                .default_value("./config.cfg"),
         )
         .get_matches();
 
     // Generate owned app arguments
     AppArgs {
-        config: String::from(matches.value_of("config").expect("invalid config value")),
+        config: matches
+            .get_one::<String>("config")
+            .expect("invalid config value")
+            .to_owned(),
     }
 }
 
